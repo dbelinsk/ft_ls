c00fe190cb66e4ab74e9c22147034c5d69ce05f4
dbelinsk <dbelinsk42@gmail.com>
2025-01-16 17:24:36 +0100

    forgotten file

--- include/ft_ls.h
+++ include/ft_ls.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ls.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dbelinsk <dbelinsk@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: dbelinsk <dbelinsk42@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 11:16:29 by dbelinsk          #+#    #+#             */
-/*   Updated: 2024/12/31 14:22:01 by dbelinsk         ###   ########.fr       */
+/*   Updated: 2025/01/16 14:44:27 by dbelinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,47 +14,51 @@
 # define FT_LS_H
 #include <stdio.h>
 #include <time.h>
+#include <sys/types.h>
 
-#define OK 0
-#define KO 1
+#define N_LINK_PAD 0
+#define UID_PAD 1
+#define GID_PAD 2
+#define SIZE_PAD 3
+#define TOTAL_BLOCK 4
 
 typedef struct s_opt{
-	int l; //1
+	int l; 
 	int a;
 	int r;
 	int R;
 	int t;
 } t_opt;
 
-typedef struct s_ls{				
-	char			*fpath;
-	char			*path;
-	char			permissions[11];
-	int				hlinks;
-	char			*owner;
-	char			*group;
-	int				size;
-	time_t			mtime;
-	char			*fname;
-	char			ftime[13];
-	struct s_ls		*next;
-	struct s_ls		*prev;
-} t_ls;
+//OPTIONS
+t_opt			*init_opt();
+int				set_opt(t_opt **opt, char *arg);
 
-void		help(void);
+//INSERT
+void			insert(char ***arr, int *size, char *fname, t_opt *opt, int sub_dir);
 
-void		path_error(char *dir);
-void		permission_error(char *dir);
-void		short_opt_error(char opt);
-void		long_opt_error(char *opt);
+//LS
+void			ft_ls(t_opt *opt, char **arr, int size);
 
-void		set_opt(t_opt **opt, char **arg);
+//INFO
+void			set_permisions(char *perm, mode_t mode);
+void			set_paddings(int *paddings, char *path, char **arr);
 
-void		insert_node(t_ls **node, t_opt *opt, char *path, char *fname);
+//PRINT
+void			print_reg_file(char *path, int *paddings, int l);
+void			print_dir(char **arr, int size, int *paddings, t_opt *opt);
+void			print_header(char *fname, int total, int l, int size);
 
+//ERRORS
+void			path_error(char *dir);
+void			permission_error(char *dir);
+int				unrecognized_option_error(char *opt);
+int				invalid_option_error(char opt);
+int				usage_error();
 
-void		print_ls(t_ls *ls, t_opt *opt);
-
-void		format_time(char *ftime, time_t mtime);
+//UTILS
+void			format_time(char *ftime, time_t mtime);
+int				get_nb_length(int nb);
+char			*ft_strappend(char *dst, char *src);
 
 #endif
