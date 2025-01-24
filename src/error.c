bf35ab74633779002b6bb6faaf3705bc665c0773
dbelinsk <dbelinsk42@gmail.com>
2025-01-24 12:04:07 +0100

    right error messges

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: dbelinsk <dbelinsk42@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 12:51:51 by dbelinsk          #+#    #+#             */
-/*   Updated: 2025/01/20 13:49:58 by dbelinsk         ###   ########.fr       */
+/*   Updated: 2025/01/24 12:00:31 by dbelinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 void		path_error(char *dir)
 {
 	errno = 1;
-	ft_putstr_fd("ls: ", 2);
+	ft_putstr_fd("./ft_ls: ", 2);
 	ft_putstr_fd(dir, 2);
 	ft_putstr_fd(": No such file or directory\n", 2);
 }
@@ -24,7 +24,7 @@ void		path_error(char *dir)
 void		permission_error(char *dir)
 {
 	errno = 1;
-	ft_putstr_fd("ls: ", 2);
+	ft_putstr_fd("./ft_ls: ", 2);
 	ft_putstr_fd(dir, 2);
 	ft_putstr_fd(": Permission denied\n", 2);
 }
@@ -32,26 +32,26 @@ void		permission_error(char *dir)
 int			unrecognized_option_error(char *opt)
 {
 	errno = 1;
-	ft_putstr_fd("ls: unrecognized option `-", 2);
+	ft_putstr_fd("./ft_ls: unrecognized option `-", 2);
 	ft_putstr_fd(opt, 2);
 	ft_putstr_fd("'\n", 2);
-	ft_putstr_fd("usage: ls [-Ralrt] [file ...]\n", 2);
+	ft_putstr_fd("usage: ./ft_ls [-Ralrt] [file ...]\n", 2);
 	return 0;
 }
 
 int			usage_error()
 {
 	errno = 1;
-	ft_putstr_fd("usage: ls [-Ralrt] [file ...]\n", 2);
+	ft_putstr_fd("usage: ./ft_ls [-Ralrt] [file ...]\n", 2);
 	return 0;
 }
 
 int			invalid_option_error(char opt)
 {
 	errno = 1;
-	ft_putstr_fd("ls: invalid option -- ", 2);
+	ft_putstr_fd("./ft_ls: invalid option -- ", 2);
 	ft_putchar_fd(opt, 2);
 	ft_putstr_fd("\n", 2);
-	ft_putstr_fd("usage: ls [-Ralrt] [file ...]\n", 2);
+	ft_putstr_fd("usage: ./ft_ls [-Ralrt] [file ...]\n", 2);
 	return 0;
 }
